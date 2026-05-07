d782a4446625f5976d16c1b61b9f6eca7484ebca
Dinesh Vankam <dineshvankam56@gmail.com>
2026-05-07 10:09:29 +0530

    Fix render deployment

--- backend/app.js
+++ backend/app.js
@@ -22,10 +22,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
-}
+//if (process.env.NODE_ENV === "production") {
+ // app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+ // app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
+//}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
